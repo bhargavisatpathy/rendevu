29654e90bc9a5f63a3db4d09127cbb16a7492445
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-03-02 12:50:53 -0700

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def new_account
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+class StaticPagesController < ApplicationController
+  # def not_found
+  #   redirect_to root_path
+  # end
+
+  def index
+  end
+
+  def think
+  end
+end
