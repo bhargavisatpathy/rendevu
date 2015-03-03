aae7be74b5d08e2a703433f65d656b1be4c2db52
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-03-02 22:09:13 -0700

    changed assets to files in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # required to serve the background image in heroku
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
